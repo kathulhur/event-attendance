b185049f7ddbe09bdf072d0dd8dc2b4f1495b73e
kathulhur <josephkarl19@outlook.com>
2021-08-31 21:17:14 +0800

    Moved the participants routes

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var eventHandler = require('../../handlers/api/eventsHandler');
-var participantHandler = require('../../handlers/api/participantsHandler');
 
-router.get('/participants', participantHandler.api.GET_participants);
-router.get('/:eventId/participants', participantHandler.api.GET_participant);
-router.post('/:eventId/participants', participantHandler.api.POST_participant);
-router.delete('/:eventId/participants/:participantId', participantHandler.api.DELETE_participant);
-router.put('/:eventId/participants/:participantId/edit', participantHandler.api.PUT_participant);
 
 router.get('/', eventHandler.api.GET_events);
 router.post('/create',  eventHandler.api.POST_eventForm);
--- routes/api/participants.js
+++ routes/api/participants.js
@@ -2,5 +2,10 @@ let express = require('express');
 let router = express.Router();
 let participantHandler = require('../../handlers/api/participantsHandler');
 
+router.get('/participants', participantHandler.api.GET_participants);
+router.get('/:eventId/participants', participantHandler.api.GET_participant);
+router.post('/:eventId/participants/create', participantHandler.api.POST_participant);
+router.delete('/:eventId/participants/:participantId/delete', participantHandler.api.DELETE_participant);
+router.put('/:eventId/participants/:participantId/edit', participantHandler.api.PUT_participant);
 
 module.exports = router;
