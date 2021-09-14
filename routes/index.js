ea7accc034ad17756f2dbca9a1d2f158a5996a0b
kathulhur <josephkarl19@outlook.com>
2021-09-14 20:21:44 +0800

    Added categorize events as ongoing, incoming, and past, then added specific route and views for each category

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,74 @@ const db = require('../lib/db');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    let events = await db.event.getLatestEvents(3);
+    let ongoingEvents = await db.event.getOngoingEvents(3);
+    let incomingEvents = await db.event.getIncomingEvents(3);
+    let pastEvents = await db.event.getPastEvents(3);
+
     return res.render('index', { 
-        events: events
+        ongoingEvents: ongoingEvents,
+        incomingEvents: incomingEvents,
+        pastEvents: pastEvents
     });
 
 });
 
-router.get('/events', async function(req, res) {
+router.get('/events', async function(req, res, next) {
     let user = db.user.getUser(req);
     try {
         let incomingEvents = await db.event.getIncomingEvents();
         let pastEvents = await db.event.getPastEvents();
+        let ongoingEvents = await db.event.getOngoingEvents();
         res.render('events', {
             incomingEvents: incomingEvents,
+            pastEvents: pastEvents,
+            ongoingEvents: ongoingEvents
+        });
+
+    } catch(err) {
+        console.error('Error: [/events]: ' + err);
+        next(err);
+    }
+});
+
+router.get('/events/ongoing', async function(req, res, next) {
+    let user = db.user.getUser(req);
+    try {
+        let ongoingEvents = await db.event.getOngoingEvents();
+        res.render('ongoing', {
+            ongoingEvents: ongoingEvents
+        });
+
+    } catch(err) {
+        console.error('Error: [/events/ongoing]: ' + err);
+        next(err);
+    }
+});
+
+router.get('/events/incoming', async function(req, res, next) {
+    let user = db.user.getUser(req);
+    try {
+        let incomingEvents = await db.event.getIncomingEvents();
+        res.render('incoming', {
+            incomingEvents: incomingEvents
+        });
+
+    } catch(err) {
+        console.error('Error: [/events/incoming]: ' + err);
+        next(err);
+    }
+});
+
+router.get('/events/past', async function(req, res, next) {
+    let user = db.user.getUser(req);
+    try {
+        let pastEvents = await db.event.getPastEvents();
+        res.render('past', {
             pastEvents: pastEvents
         });
 
     } catch(err) {
-        console.err('Error: [/events]: ' + err);
+        console.error('Error: [/events/past]: ' + err);
         next(err);
     }
 });
